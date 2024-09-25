d83ed6d1e603897da66af636d105ab4e7ab5be2d
jyassien <jihadumuhammed@gmail.com>
2024-09-24 21:33:38 -0600

    Email address string updated to be case insensitive.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
     validates :first_name, :last_name, :school_email, :major, :graduation_date, presence: true # Values can't be empty
 
     
-    validates :school_email, uniqueness: true   #Email has to be unique.
+    validates :school_email, uniqueness: { case_sensitive: false }   # Email has to be unique.
     validates :school_email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is not a valid email." }
     validates :school_email, format: { with: /\A[\w+\-.]+@msudenver\.edu\z/i, message: "address is not a valid 'username@msudenver.edu' email." }
 
