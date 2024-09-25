2ba8e5959120ffd3481237a3b8d1be0593682f50
jyassien <jihadumuhammed@gmail.com>
2024-09-24 20:19:20 -0600

    @msudenver.edu email validation added.

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,6 @@ class Student < ApplicationRecord
     
     validates :school_email, uniqueness: true   #Email has to be unique.
 
+    validates :school_email, format: { with: /\A[\w+\-.]+@msudenver\.edu\z/i, message: "address is not a valid 'username@msudenver.edu' email." }
+
 end
